39a5aeaa68f27894b079ab75a60513743ceac192
Izunyaaan <kanebluerivers@gmail.com>
2026-07-19 17:40:05 +0800

    added skeleton for netlify serverless function. So can use netlify env for sensitive data :3

---
+++ netlify/functions/fetch-data.js
@@ -0,0 +1,9 @@
+export async function handler(event, context) {
+    try {
+        const firebaseApiKey = process.env.FIREBASE_API_KEY;
+        // insert rest of logic here later once firebase is setup
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
